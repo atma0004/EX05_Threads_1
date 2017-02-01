9dd6b5621e66be0113d59b482bc74f0f38691101
atma0004 <atma0004@edu.eal.dk>
2017-02-01 21:55:16 +0100

    Add project files.

---
+++ EX05_Threads_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace EX05_Threads_1
+    {
+    class Program
+        {
+        static void Main(string[] args)
+            {
+            Thread firstThread = new Thread(new ThreadStart(FirstThread));
+            firstThread.Start();
+
+            }
+      static void FirstThread()
+            {
+
+            for (int i = 0; i<5; i++)
+                {
+                Console.WriteLine("C# thread is easy");
+                Thread.Sleep(2000);
+
+                }
+            Console.ReadKey();
+            }
+        }
+    }
